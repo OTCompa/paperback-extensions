d8fc61b1f43f42deb68fe511ca9231723c2c5126
OTCompa <otcompa@gmail.com>
2025-11-18 05:51:10 -0600

    filter bugged manga from section lists

--- src/MangaDex/MangaDexParser.ts
+++ src/MangaDex/MangaDexParser.ts
@@ -2,6 +2,7 @@ import { PartialSourceManga } from '@paperback/types'
 import { MDImageQuality } from './MangaDexHelper'
 import { MangaItem } from './MangaDexInterfaces'
 
+const BUGGED_MANGA = ["dfd5d526-9f4e-48b2-82eb-9d4c267433b7"]
 
 export const parseMangaList = async (object: MangaItem[], source: any, thumbnailSelector: any): Promise<PartialSourceManga[]> => {
     const results: PartialSourceManga[] = []
@@ -14,14 +15,16 @@ export const parseMangaList = async (object: MangaItem[], source: any, thumbnail
         const image = coverFileName ? `${source.COVER_BASE_URL}/${mangaId}/${coverFileName}${MDImageQuality.getEnding(await thumbnailSelector(source.stateManager))}` : 'https://mangadex.org/_nuxt/img/cover-placeholder.d12c3c5.jpg'
         const subtitle = `${mangaDetails.lastVolume ? `Vol. ${mangaDetails.lastVolume}` : ''} ${mangaDetails.lastChapter ? `Ch. ${mangaDetails.lastChapter}` : ''}`
 
-        results.push(
-            App.createPartialSourceManga({
-                mangaId: mangaId,
-                title: title,
-                image: image,
-                subtitle: subtitle
-            })
-        )
+        if (BUGGED_MANGA.indexOf(mangaId) == -1) {
+            results.push(
+                App.createPartialSourceManga({
+                    mangaId: mangaId,
+                    title: title,
+                    image: image,
+                    subtitle: subtitle
+                })
+            )
+        }
     }
     return results
 }
